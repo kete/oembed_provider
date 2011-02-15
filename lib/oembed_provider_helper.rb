569f0c937f6227a8cd4cd08cf045f0acfacc929a
Walter McGinnis <walter@katipo.co.nz>
2011-02-16 10:55:09 +1300

    Version bump to 0.1.0

---
+++ lib/oembed_provider_helper.rb
@@ -0,0 +1,18 @@
+# include this in your relevant helpers
+# to add discoverability link, etc.
+module OembedProviderHelper
+  # hardcodes http as protocol
+  # http is specified in http://oembed.com/
+  def oembed_provider_links
+    host_url = request.host
+    escaped_request_url = request.url.sub('://', '%3A//')
+    html = tag(:link, :rel => "alternate",
+               :type => "application/json+oembed",
+               :href => "http://#{host_url}/oembed?url=#{escaped_request_url}",
+               :title => "JSON oEmbed for #{@title}")
+    html += tag(:link, :rel => "alternate",
+               :type => "application/xml+oembed",
+               :href => "http://#{host_url}/oembed?url=#{escaped_request_url}&format=xml",
+               :title => "XML oEmbed for #{@title}")
+  end
+end
